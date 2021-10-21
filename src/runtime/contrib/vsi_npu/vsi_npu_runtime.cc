2889bf9bc74feac32507f7a888364c58d1951654
xiang.zhang <xiang.zhang@verisilicon.com>
2021-10-21 18:50:58 +0800

    Fix user after free issue in multi-thread context
    Signed-off-by: xiang.zhang <xiang.zhang@verisilicon.com>

--- src/runtime/contrib/vsi_npu/vsi_npu_runtime.cc
+++ src/runtime/contrib/vsi_npu/vsi_npu_runtime.cc
@@ -18,10 +18,12 @@
  */
 #include "vsi_npu_runtime.h"
 
-#include <sys/time.h>
-#include <time.h>
-
 #include <dmlc/memory_io.h>
+#include <sys/time.h>
+#include <tim/vx/operation.h>
+#include <tim/vx/ops/nbg.h>
+#include <tim/vx/tensor.h>
+#include <time.h>
 #include <tvm/runtime/c_runtime_api.h>
 #include <tvm/runtime/memory.h>
 #include <tvm/runtime/module.h>
@@ -29,66 +31,63 @@
 #include <tvm/runtime/packed_func.h>
 #include <tvm/runtime/registry.h>
 
-#include <tim/vx/operation.h>
-#include <tim/vx/tensor.h>
-#include <tim/vx/ops/nbg.h>
-
-#define BILLION                                 1000000000
-static uint64_t get_perf_count()
-{
+#define BILLION 1000000000
+static uint64_t get_perf_count() {
 #if defined(__linux__) || defined(__ANDROID__) || defined(__QNX__) || defined(__CYGWIN__)
-    struct timespec ts;
+  struct timespec ts;
 
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+  clock_gettime(CLOCK_MONOTONIC, &ts);
 
-    return (uint64_t)((uint64_t)ts.tv_nsec + (uint64_t)ts.tv_sec * BILLION);
+  return (uint64_t)((uint64_t)ts.tv_nsec + (uint64_t)ts.tv_sec * BILLION);
 #elif defined(_WIN32) || defined(UNDER_CE)
-    LARGE_INTEGER ln;
+  LARGE_INTEGER ln;
 
-    QueryPerformanceCounter(&ln);
+  QueryPerformanceCounter(&ln);
 
-    return (uint64_t)ln.QuadPart;
+  return (uint64_t)ln.QuadPart;
 #endif
 }
 
 namespace tvm {
 namespace runtime {
 namespace vsi_npu {
-PackedFunc VsiNpuModule::GetFunction(const std::string& name, const ObjectPtr<Object>& sptr_to_self) {
+PackedFunc VsiNpuModule::GetFunction(const std::string& name,
+                                     const ObjectPtr<Object>& sptr_to_self) {
   std::string vsi_graph_name = "tvmgen_default_vsi_npu_0";
   std::cout << "VsiNpuModule::GetFunction: " << name << std::endl;
-  if (name != vsi_graph_name){
-  std::cout << "VsiNpuModule::GetFunction: return early" << std::endl;
+  if (name != vsi_graph_name) {
+    std::cout << "VsiNpuModule::GetFunction: return early" << std::endl;
     return PackedFunc();
   }
 
   // TODO: only handle name = "vsi_npu_*", but now we can not see vsi_npu_* invoked
   return PackedFunc([sptr_to_self, this, name](TVMArgs args, TVMRetValue* rv) {
-    this->vx_context_ = tim::vx::Context::Create();
-    this->vx_graph_ = this->vx_context_->CreateGraph();
+    auto vx_context_ = tim::vx::Context::Create();
+    auto vx_graph_ = vx_context_->CreateGraph();
+    auto compiled_nbg_ = this->compiled_nbg_;
+    auto inputs_ = this->inputs_;
+    auto outputs_ = this->outputs_;
 
-    // auto nbg_node = std::make_shared<tim::vx::Operation>(
-    //     this->vx_graph_.get(), 68, this->inputs_.size(), this->outputs_.size());
-    auto nbg_node = this->vx_graph_->CreateOperation<tim::vx::ops::NBG>(
-        this->compiled_nbg_.get(), this->inputs_.size(), this->outputs_.size());
+    auto nbg_node = vx_graph_->CreateOperation<tim::vx::ops::NBG>(compiled_nbg_.get(),
+                                                                  inputs_.size(), outputs_.size());
 
     std::vector<std::shared_ptr<tim::vx::Tensor>> inputs, outputs;
 
     std::transform(
-        this->inputs_.begin(), this->inputs_.end(), std::back_inserter(inputs),
-        [this](const tim::vx::TensorSpec& spec) { return this->vx_graph_->CreateTensor(spec); });
+        inputs_.begin(), inputs_.end(), std::back_inserter(inputs),
+        [vx_graph_](const tim::vx::TensorSpec& spec) { return vx_graph_->CreateTensor(spec); });
 
     std::transform(
-        this->outputs_.begin(), this->outputs_.end(), std::back_inserter(outputs),
-        [this](const tim::vx::TensorSpec& spec) { return this->vx_graph_->CreateTensor(spec); });
+        outputs_.begin(), outputs_.end(), std::back_inserter(outputs),
+        [vx_graph_](const tim::vx::TensorSpec& spec) { return vx_graph_->CreateTensor(spec); });
 
     (*nbg_node).BindInputs(inputs).BindOutputs(outputs);
 
-    this->vx_graph_->Compile();
+    vx_graph_->Compile();
     // prepare input
-    std::vector<DLTensor*> in_tensors_tvm(this->inputs_.size());
+    std::vector<DLTensor*> in_tensors_tvm(inputs_.size());
     uint8_t argc = 0;
-    for (uint32_t i = 0; i < this->inputs_.size(); i++) {
+    for (uint32_t i = 0; i < inputs_.size(); i++) {
       in_tensors_tvm[i] = args[argc++];
       inputs[i]->CopyDataToTensor(static_cast<uint8_t*>(in_tensors_tvm[i]->data),
                                   GetDataSize(*(in_tensors_tvm[i])));
@@ -96,16 +95,16 @@ PackedFunc VsiNpuModule::GetFunction(const std::string& name, const ObjectPtr<Ob
 
     uint64_t tmsStart, tmsEnd, msVal, usVal;
 
-    this->vx_graph_->Run();  // warmup NPU, NPU maybe poweroff
+    vx_graph_->Run();  // warmup NPU, NPU maybe poweroff
     tmsStart = get_perf_count();
-    this->vx_graph_->Run();
+    vx_graph_->Run();
     tmsEnd = get_perf_count();
     msVal = (tmsEnd - tmsStart) / 1000000;
     usVal = (tmsEnd - tmsStart) / 1000;
     printf("Process Graph: %ld ms or %ld us\n", msVal, usVal);
     // get output
-    std::cout << "VsiNpuModule::GetFunction: size: " << args.size() <<std::endl;
-    for (uint32_t i = 0; i < this->outputs_.size(); i++) {
+    std::cout << "VsiNpuModule::GetFunction: size: " << args.size() << std::endl;
+    for (uint32_t i = 0; i < outputs_.size(); i++) {
       DLTensor* out_tensor_tvm = args[argc++];
       outputs[i]->CopyDataFromTensor(static_cast<uint8_t*>(out_tensor_tvm->data));
     }
@@ -113,10 +112,10 @@ PackedFunc VsiNpuModule::GetFunction(const std::string& name, const ObjectPtr<Ob
 }
 
 void VsiNpuModule::SerializeTensorSpec(tim::vx::TensorSpec& t_spec, std::ostream& out) {
-  std::cout<<"VsiNpuModule : SerializeTensorSpec"<<std::endl;
+  std::cout << "VsiNpuModule : SerializeTensorSpec" << std::endl;
   TensorSpecIR t_spec_ir;
   t_spec_ir.quant_type = t_spec.quantization_.Type();
-  t_spec_ir.channel_dim =  t_spec.quantization_.ChannelDim();
+  t_spec_ir.channel_dim = t_spec.quantization_.ChannelDim();
   t_spec_ir.scales = t_spec.quantization_.Scales();
   t_spec_ir.zps = t_spec.quantization_.ZeroPoints();
 
@@ -129,46 +128,52 @@ void VsiNpuModule::SerializeTensorSpec(tim::vx::TensorSpec& t_spec, std::ostream
   uint64_t scales_size = t_spec_ir.scales.size();
   out.write(reinterpret_cast<const char*>(&scales_size), sizeof(scales_size));
   if (scales_size > 0) {
-    out.write(reinterpret_cast<const char*>(t_spec_ir.scales.data()), sizeof(t_spec_ir.scales[0]) * scales_size);
-    out.write(reinterpret_cast<const char*>(t_spec_ir.zps.data()), sizeof(t_spec_ir.zps[0]) * scales_size);
+    out.write(reinterpret_cast<const char*>(t_spec_ir.scales.data()),
+              sizeof(t_spec_ir.scales[0]) * scales_size);
+    out.write(reinterpret_cast<const char*>(t_spec_ir.zps.data()),
+              sizeof(t_spec_ir.zps[0]) * scales_size);
   }
   out.write(reinterpret_cast<const char*>(&t_spec_ir.data_type), sizeof(t_spec_ir.data_type));
   uint64_t shape_size = t_spec_ir.shape.size();
   out.write(reinterpret_cast<const char*>(&shape_size), sizeof(shape_size));
   if (shape_size > 0) {
-    out.write(reinterpret_cast<const char*>(t_spec_ir.shape.data()), sizeof(t_spec_ir.shape[0]) * shape_size);
+    out.write(reinterpret_cast<const char*>(t_spec_ir.shape.data()),
+              sizeof(t_spec_ir.shape[0]) * shape_size);
   }
   out.write(reinterpret_cast<const char*>(&t_spec_ir.attr), sizeof(t_spec_ir.attr));
-  std::cout<<"VsiNpuModule : SerializeTensorSpec2"<<std::endl;
+  std::cout << "VsiNpuModule : SerializeTensorSpec2" << std::endl;
 }
 
- tim::vx::TensorSpec VsiNpuModule::DeSerializeTensorSpec(std::istream& in) {
-   std::cout<<"VsiNpuModule : DeSerializeTensorSpec"<<std::endl;
-   TensorSpecIR t_spec_ir;
-   in.read(reinterpret_cast<char*>(&t_spec_ir.quant_type), sizeof(t_spec_ir.quant_type));
-   in.read(reinterpret_cast<char*>(&t_spec_ir.channel_dim), sizeof(t_spec_ir.channel_dim));
-   uint64_t scales_size = 0;
-   in.read(reinterpret_cast<char*>(&scales_size), sizeof(scales_size));
-   if (scales_size > 0) {
-     t_spec_ir.scales.resize(scales_size);
-     t_spec_ir.zps.resize(scales_size);
-     in.read(reinterpret_cast<char*>(t_spec_ir.scales.data()), sizeof(t_spec_ir.scales[0]) * scales_size);
-     in.read(reinterpret_cast<char*>(t_spec_ir.zps.data()), sizeof(t_spec_ir.zps[0]) * scales_size);
-   }
-   in.read(reinterpret_cast<char*>(&t_spec_ir.data_type), sizeof(t_spec_ir.data_type));
-   uint64_t shape_size = 0;
-   in.read(reinterpret_cast<char*>(&shape_size), sizeof(shape_size));
-   if (shape_size > 0) {
-     t_spec_ir.shape.resize(shape_size);
-     in.read(reinterpret_cast<char*>(t_spec_ir.shape.data()), sizeof(t_spec_ir.shape[0]) * shape_size);
-   }
-   in.read(reinterpret_cast<char*>(&t_spec_ir.attr), sizeof(t_spec_ir.attr));
+tim::vx::TensorSpec VsiNpuModule::DeSerializeTensorSpec(std::istream& in) {
+  std::cout << "VsiNpuModule : DeSerializeTensorSpec" << std::endl;
+  TensorSpecIR t_spec_ir;
+  in.read(reinterpret_cast<char*>(&t_spec_ir.quant_type), sizeof(t_spec_ir.quant_type));
+  in.read(reinterpret_cast<char*>(&t_spec_ir.channel_dim), sizeof(t_spec_ir.channel_dim));
+  uint64_t scales_size = 0;
+  in.read(reinterpret_cast<char*>(&scales_size), sizeof(scales_size));
+  if (scales_size > 0) {
+    t_spec_ir.scales.resize(scales_size);
+    t_spec_ir.zps.resize(scales_size);
+    in.read(reinterpret_cast<char*>(t_spec_ir.scales.data()),
+            sizeof(t_spec_ir.scales[0]) * scales_size);
+    in.read(reinterpret_cast<char*>(t_spec_ir.zps.data()), sizeof(t_spec_ir.zps[0]) * scales_size);
+  }
+  in.read(reinterpret_cast<char*>(&t_spec_ir.data_type), sizeof(t_spec_ir.data_type));
+  uint64_t shape_size = 0;
+  in.read(reinterpret_cast<char*>(&shape_size), sizeof(shape_size));
+  if (shape_size > 0) {
+    t_spec_ir.shape.resize(shape_size);
+    in.read(reinterpret_cast<char*>(t_spec_ir.shape.data()),
+            sizeof(t_spec_ir.shape[0]) * shape_size);
+  }
+  in.read(reinterpret_cast<char*>(&t_spec_ir.attr), sizeof(t_spec_ir.attr));
 
-   tim::vx::Quantization quant(t_spec_ir.quant_type, t_spec_ir.channel_dim, t_spec_ir.scales, t_spec_ir.zps);
-   tim::vx::TensorSpec t_spec(t_spec_ir.data_type, t_spec_ir.shape, t_spec_ir.attr, quant);
-   std::cout<<"VsiNpuModule : DeSerializeTensorSpec2"<<std::endl;
-   return t_spec;
- }
+  tim::vx::Quantization quant(t_spec_ir.quant_type, t_spec_ir.channel_dim, t_spec_ir.scales,
+                              t_spec_ir.zps);
+  tim::vx::TensorSpec t_spec(t_spec_ir.data_type, t_spec_ir.shape, t_spec_ir.attr, quant);
+  std::cout << "VsiNpuModule : DeSerializeTensorSpec2" << std::endl;
+  return t_spec;
+}
 
 void VsiNpuModule::SaveToBinary(dmlc::Stream* stream) {
   // todo: we need save input/output information to stream
@@ -202,7 +207,7 @@ Module VsiNpuModule::LoadFromBinary(void* strm) {
   uint32_t nbg_size;
   stream->Read(&nbg_size);
   std::cout << __FUNCTION__ << ": nbg size = " << nbg_size << std::endl;
-  std::shared_ptr<char> nbg_buf(new char [nbg_size]); // TODO: memory leak risk, need double confirm
+  std::shared_ptr<char> nbg_buf(new char[nbg_size]);  // TODO: memory leak risk, need double confirm
   stream->Read(nbg_buf.get(), nbg_size);
 
   uint64_t input_size;
@@ -234,7 +239,7 @@ Module VsiNpuModule::LoadFromBinary(void* strm) {
 }
 
 TVM_REGISTER_GLOBAL("runtime.module.loadbinary_vsi_npu")
-    .set_body([](TVMArgs args, TVMRetValue* rv) { *rv = VsiNpuModule::LoadFromBinary(args[0]);});
+    .set_body([](TVMArgs args, TVMRetValue* rv) { *rv = VsiNpuModule::LoadFromBinary(args[0]); });
 
 }  // namespace vsi_npu
 }  // namespace runtime
